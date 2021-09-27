f3aaf2702e822355e4c90c292cd1776991091104
hugo <hugomendonca9@gmail.com>
2021-09-26 22:10:51 -0300

    password reset

--- endpoints/password.php
+++ endpoints/password.php
@@ -24,6 +24,11 @@
             return rest_ensure_response($response);
         }
 
+        if(empty($url)){
+            $response = new WP_Error('error', 'Url inválida.', ['status' => 401]);
+            return rest_ensure_response($response);
+        }
+
         // Pegando o login do usuário e email.
         $user_login = $user->user_login;
         $user_email = $user->user_email;
@@ -52,5 +57,41 @@
     }
 
     add_action('rest_api_init', 'register_api_password_lost');
+
+    // Password Reset
+
+    function api_password_reset($request) {
+        $login = $request['login'];
+        $password = $request['password'];
+        $key = $request['key'];
+        $user = get_user_by('login', $login);
+
+        if(empty($user)){
+            $response = new WP_Error('error', 'Usuário não existe.', ['status' => 401]);
+            return rest_ensure_response($response);
+        }
+
+        $check_key = check_password_reset_key($key, $login);
+
+        if(is_wp_error($check_key)){
+            $response = new WP_Error('error', 'Token expirado.', ['status' => 401]);
+            return rest_ensure_response($response);
+        }
+
+        reset_password($user, $password);
+
+        return rest_ensure_response('Senha alterada com sucesso!');
+    }
+
+
+    // Registrando a função de delete na rota.
+    function register_api_password_reset() {
+        register_rest_route('api', '/password/reset', [
+            'methods' => WP_REST_Server::CREATABLE,
+            'callback' => 'api_password_reset',
+        ]);
+    }
+
+    add_action('rest_api_init', 'register_api_password_reset');
 
 ?>
